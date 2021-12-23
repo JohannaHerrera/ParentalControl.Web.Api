b0fbac6d7c168287a054049f7c8ecb2659f14052
Johanna Herrera <johanna.herrera1a@gmail.com>
2021-12-22 19:17:45 -0500

    Home/News

---
+++ ParentalControl.Web.Api/Controllers/NewsController.cs
@@ -0,0 +1,58 @@
+﻿using ParentalControl.Web.Api.Data;
+using ParentalControl.Web.Api.Models.EntityModels;
+using ParentalControl.Web.Api.Models.ReponseModels;
+using System;
+using System.Net;
+using System.Threading;
+using System.Web.Http;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ParentalControl.Web.Api.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/News")]
+    public class NewsController : ApiController
+    {
+        [HttpGet]
+        public NewsResponseModel Get()
+        {
+            NewsResponseModel newsResponseModel = new NewsResponseModel();
+            List<NewsModel> newsModelList = new List<NewsModel>();
+
+            try
+            {
+                using(var db = new ParentalControlDBEntities())
+                {
+                    var newsInfo = (from ns in db.News
+                                    select ns).ToList();
+
+                    if(newsInfo.Count() > 0)
+                    {
+                        foreach (var item in newsInfo)
+                        {
+                            NewsModel newsModel = new NewsModel();
+                            newsModel.NewsTitle = item.NewsTitle;
+                            newsModel.NewsDescription = item.NewsDescription;
+                            newsModel.NewsLink = item.NewsLink;
+                            newsModelList.Add(newsModel);
+                        }
+
+                        newsResponseModel.NewsModelList = newsModelList; 
+                    }
+                    else
+                    {
+                        newsResponseModel.MessageError = "No se obtuvo información sobre las últimas noticias.";
+                    }
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                newsResponseModel.MessageError = "Ha ocurrido un error inesperado.";
+            }
+
+            return newsResponseModel;
+        }
+    }
+}
---
+++ ParentalControl.Web.Api/Models/EntityModels/NewsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParentalControl.Web.Api.Models.EntityModels
+{
+    public class NewsModel
+    {
+        public string NewsTitle { get; set; }
+        public string NewsDescription { get; set; }
+        public string NewsLink { get; set; }
+    }
+}
