6b5831347e925d884a48add1f4b952ac4e3f9fcf
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2021-12-26 23:15:59 -0500

    Schedule

--- ParentalControl.Web.Api/Controllers/ScheduleController.cs
+++ ParentalControl.Web.Api/Controllers/ScheduleController.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using ParentalControl.Web.Api.Models.ReponseModels;
 using ParentalControl.Web.Api.Models.EntityModels;
 using ParentalControl.Web.Api.Data;
+using ParentalControl.Web.Api.Constants;
+using System.Data.Entity; 
 
 namespace ParentalControl.Web.Api.Controllers
 {
@@ -13,6 +15,47 @@ namespace ParentalControl.Web.Api.Controllers
     [RoutePrefix("api/Schedule")]
     public class ScheduleController : ApiController
     {
-    
+        //SOLO nombre post
+        // otro controlador
+
+
+        [HttpPost]
+        public List<ScheduleResponseModel> Post([FromBody] GetScheduleInfoModel getScheduleInfoModel)
+        {
+            List<ScheduleResponseModel> scheduleResponseModelList = new List<ScheduleResponseModel>();
+            ScheduleResponseModel scheduleResponseModel = new ScheduleResponseModel();
+            try
+            {
+                using (var db = new ParentalControlDBEntities())
+                {
+                    var scheduleInfoList = (from schedule in db.Schedule
+                                            where schedule.ParentId == getScheduleInfoModel.ParentId
+                                            select schedule).ToList();
+                    if (scheduleInfoList.Count > 0)
+                    {
+                        foreach (var item in scheduleInfoList)
+                        {
+                            scheduleResponseModel.ScheduleId = item.ScheduleId;
+                            scheduleResponseModel.ScheduleStartTime = item.ScheduleStartTime;
+                            scheduleResponseModel.ScheduleEndTime = item.ScheduleEndTime;
+                            scheduleResponseModel.ParentId = item.ParentId;
+                            scheduleResponseModelList.Add(scheduleResponseModel);
+                        }
+                    }
+                    else
+                    {
+                        scheduleResponseModel.MessageError = "No se encontró información de horarios.";
+                        scheduleResponseModelList.Add(scheduleResponseModel);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                scheduleResponseModel.MessageError = "Ocurrió un error inesperado. Inténtelo de nuevo.";
+                scheduleResponseModelList.Add(scheduleResponseModel);
+            }
+            return scheduleResponseModelList;
+        }
+        
     }
 }
--- ParentalControl.Web.Api/Models/ReponseModels/ScheduleResponseModel.cs
+++ ParentalControl.Web.Api/Models/ReponseModels/ScheduleResponseModel.cs
@@ -10,7 +10,10 @@ namespace ParentalControl.Web.Api.Models.ReponseModels
         public int ScheduleId { get; set; }
         public DateTime ScheduleStartTime { get; set; }
         public DateTime ScheduleEndTime { get; set; }
-        public DateTime ScheduleCreationDate { get; set; }
+        //public DateTime ScheduleCreationDate { get; set; }
         public int? ParentId { get; set; }
+        public string MessageError { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool Registered { get; set; }
     }
 }
