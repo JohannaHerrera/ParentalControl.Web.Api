d0460186ffb1d537196b0d241021a964d0d3833a
Johanna Herrera <johanna.herrera1a@gmail.com>
2021-12-28 12:11:38 -0500

    My Profile.

---
+++ ParentalControl.Web.Api/Controllers/ParentController.cs
@@ -0,0 +1,91 @@
+﻿using ParentalControl.Web.Api.Data;
+using ParentalControl.Web.Api.Models.EntityModels;
+using ParentalControl.Web.Api.Models.ReponseModels;
+using System;
+using System.Net;
+using System.Threading;
+using System.Web.Http;
+using System.Linq;
+
+namespace ParentalControl.Web.Api.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/Login")]
+    public class ParentController : ApiController
+    {
+        [HttpPost]
+        public GetMyProfileResponseModel Post([FromBody] int parentId)
+        {
+            GetMyProfileResponseModel getMyProfileResponseModel = new GetMyProfileResponseModel();
+
+            try
+            {
+                if(parentId > 0)
+                {
+                    using(var db = new ParentalControlDBEntities())
+                    {
+                        var parentInfo = (from parent in db.Parent
+                                          where parent.ParentId == parentId
+                                          select parent).FirstOrDefault();
+
+                        if(parentInfo != null)
+                        {
+                            getMyProfileResponseModel.ParentUsername = parentInfo.ParentUsername;
+                            getMyProfileResponseModel.ParentEmail = parentInfo.ParentEmail;
+                            getMyProfileResponseModel.ParentPassword = parentInfo.ParentPassword;
+                        }
+                    }
+                }
+                else
+                {
+                    getMyProfileResponseModel.MessageError = "No se pudo encontrar su información personal.";
+                }
+            }
+            catch (Exception ex)
+            {
+                getMyProfileResponseModel.MessageError = "Ocurrió un error inesperado. Inténtelo de nuevo.";
+            }
+
+            return getMyProfileResponseModel;
+        }
+
+        [HttpPut]
+        public bool Put([FromBody] MyProfileModel myProfileModel)
+        {
+            bool result = false;
+
+            try
+            {
+                if (myProfileModel != null && myProfileModel.ParentId > 0)
+                {
+                    using (var db = new ParentalControlDBEntities())
+                    {
+                        var parentInfo = (from parent in db.Parent
+                                          where parent.ParentId == myProfileModel.ParentId
+                                          select parent).FirstOrDefault();
+
+                        if (parentInfo != null)
+                        {
+                            parentInfo.ParentUsername = myProfileModel.ParentUsername;
+                            parentInfo.ParentEmail = myProfileModel.ParentEmail;
+                            parentInfo.ParentPassword = myProfileModel.ParentPassword;
+                            db.SaveChanges();
+
+                            result = true;
+                        }
+                    }
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ParentalControl.Web.Api/Models/ReponseModels/GetMyProfileResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParentalControl.Web.Api.Models.ReponseModels
+{
+    public class GetMyProfileResponseModel
+    {
+        public string ParentUsername { get; set; }
+        public string ParentEmail { get; set; }
+        public string ParentPassword { get; set; }
+        public string MessageError { get; set; }
+    }
+}
