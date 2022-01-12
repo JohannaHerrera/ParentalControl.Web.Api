663050994cbcd8348b0ec66d3d8d3e6ba2c992f6
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2022-01-11 21:52:33 -0500

    Bloqueo Web

---
+++ ParentalControl.Web.Api/Models/ReponseModels/WebConfigurationResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ParentalControl.Web.Api.Models.EntityModels;
+
+namespace ParentalControl.Web.Api.Models.ReponseModels
+{
+    public class WebConfigurationResponseModel
+    {
+        public string MessageError { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
